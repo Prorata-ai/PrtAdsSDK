59edae2906cc10bc7847e28e9eda898cc94f9cf5
joe-prorata <joe@prorata.ai>
2025-12-03 11:33:56 -0800

    Add sandbox to iFrame

--- swift-sdk/Sources/GistAdsSDK/Utils/IframeHTMLGenerator.swift
+++ swift-sdk/Sources/GistAdsSDK/Utils/IframeHTMLGenerator.swift
@@ -24,7 +24,8 @@ enum IframeHTMLGenerator {
         frameborder="0" 
         scrolling="no" 
         allowfullscreen 
-        allow="autoplay; fullscreen; payment" 
+        allow="autoplay; fullscreen; payment"
+        sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-popups-to-escape-sandbox"
         style="width:100%;height:100%;min-height:250px;border:none;">
 </iframe>
 """
